f2d90f2f8c0cd5062e421bf2ea84e2989396ea20
Shivam Jaiswal <93713364+shivamj2001@users.noreply.github.com>
2025-04-19 08:28:49 +0000

    added code

---
+++ dagster_essentials/assets/metrics.py
@@ -0,0 +1,117 @@
+import dagster as dg
+
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+import duckdb
+import os
+from dagster_duckdb import DuckDBResource
+
+from datetime import datetime, timedelta
+from dagster_essentials.assets import constants
+
+import pandas as pd
+from dagster_essentials.partitions import weekly_partition
+
+import dagster as dg
+from dagster._utils.backoff import backoff
+
+from dagster_essentials.assets import constants
+
+
+@dg.asset(
+    deps=["taxi_trips", "taxi_zones"]
+)
+def manhattan_stats(database: DuckDBResource) -> None:
+    query = """
+        select
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        from trips
+        left join zones on trips.pickup_zone_id = zones.zone_id
+        where borough = 'Manhattan' and geometry is not null
+        group by zone, borough, geometry
+    """
+
+    with database.get_connection() as conn:
+        trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(constants.MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
+        output_file.write(trips_by_zone.to_json())
+
+
+
+
+        
+
+@dg.asset(
+    deps=["manhattan_stats"],
+)
+def manhattan_map() -> None:
+    trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    trips_by_zone.plot(column="num_trips", cmap="plasma", legend=True, ax=ax, edgecolor="black")
+    ax.set_title("Number of Trips per Taxi Zone in Manhattan")
+
+    ax.set_xlim([-74.05, -73.90])  # Adjust longitude range
+    ax.set_ylim([40.70, 40.82])  # Adjust latitude range
+
+    # Save the image
+    plt.savefig(constants.MANHATTAN_MAP_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
+
+
+
+
+
+@dg.asset(
+    deps=["taxi_trips"],
+    partitions_def=weekly_partition
+)
+def trips_by_week(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
+    """
+      The number of trips per week, aggregated by week.
+    """
+
+    period_to_fetch = context.partition_key
+
+    # get all trips for the week
+    query = f"""
+        select vendor_id, total_amount, trip_distance, passenger_count
+        from trips
+        where pickup_datetime >= '{period_to_fetch}'
+            and pickup_datetime < '{period_to_fetch}'::date + interval '1 week'
+    """
+
+    with database.get_connection() as conn:
+        data_for_month = conn.execute(query).fetch_df()
+
+    aggregate = data_for_month.agg({
+        "vendor_id": "count",
+        "total_amount": "sum",
+        "trip_distance": "sum",
+        "passenger_count": "sum"
+    }).rename({"vendor_id": "num_trips"}).to_frame().T # type: ignore
+
+    # clean up the formatting of the dataframe
+    aggregate["period"] = period_to_fetch
+    aggregate['num_trips'] = aggregate['num_trips'].astype(int)
+    aggregate['passenger_count'] = aggregate['passenger_count'].astype(int)
+    aggregate['total_amount'] = aggregate['total_amount'].round(2).astype(float)
+    aggregate['trip_distance'] = aggregate['trip_distance'].round(2).astype(float)
+    aggregate = aggregate[["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]]
+
+    try:
+        # If the file already exists, append to it, but replace the existing month's data
+        existing = pd.read_csv(constants.TRIPS_BY_WEEK_FILE_PATH)
+        existing = existing[existing["period"] != period_to_fetch]
+        existing = pd.concat([existing, aggregate]).sort_values(by="period")
+        existing.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
+    except FileNotFoundError:
+        aggregate.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
---
+++ dagster_essentials/assets/trips.py
@@ -0,0 +1,100 @@
+import duckdb
+import os
+import dagster as dg
+from dagster_duckdb import DuckDBResource
+from dagster._utils.backoff import backoff
+import requests
+from dagster_essentials.assets import constants
+import dagster as dg
+
+
+from dagster_essentials.partitions import monthly_partition
+
+@dg.asset(
+    partitions_def=monthly_partition
+)
+def taxi_trips_file(context: dg.AssetExecutionContext) -> None:
+  """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+  """
+
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
+
+  raw_trips = requests.get(
+      f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+  )
+
+  with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+      output_file.write(raw_trips.content)
+
+
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+      The raw parquet files for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    """month_to_fetch = '2023-02'"""
+    raw_taxi_zones = requests.get(
+        f"https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
+
+
+
+@dg.asset(
+    deps=["taxi_trips_file"],
+    partitions_def=monthly_partition,
+)
+def taxi_trips(context: dg.AssetExecutionContext, database: DuckDBResource) -> None:
+  """
+    The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month.
+  """
+
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
+
+  query = f"""
+    create table if not exists trips (
+      vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+      rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+      pickup_datetime timestamp, trip_distance double, passenger_count double,
+      total_amount double, partition_date varchar
+    );
+
+    delete from trips where partition_date = '{month_to_fetch}';
+
+    insert into trips
+    select
+      VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+      tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+    from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+  """
+
+  with database.get_connection() as conn:
+      conn.execute(query)
+
+
+
+@dg.asset(
+    deps=["taxi_zones_file"]
+)
+def taxi_zones(database: DuckDBResource) -> None:
+
+    query = f"""
+        create or replace table zones as (
+          select
+            LocationID as zone_id,
+            Borough as borough,
+            zone,
+            the_geom as geometry
+          from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(query)
+
